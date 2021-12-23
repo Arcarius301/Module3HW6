b0bf01341404fbf219867461bcae45e8e4509600
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2021-12-23 17:39:57 +0200

    Done.

---
+++ Module3HW6/MessageBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW6
+{
+    public class MessageBox
+    {
+        public event Action<State> OnClosed;
+
+        public async void Open()
+        {
+            var random = new Random();
+            Console.WriteLine("Открыто окно");
+            await Task.Delay(3000);
+            Console.WriteLine("Окно было закрыто пользователем");
+
+            OnClosed?.Invoke((State)random.Next(2));
+        }
+    }
+}
--- Module3HW6/Program.cs
+++ Module3HW6/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Module3HW6
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly TaskCompletionSource _tcs = new TaskCompletionSource();
+
+        public static async Task Main(string[] args)
         {
+            var messageBox = new MessageBox();
+            messageBox.OnClosed += DisplayMessage;
+            messageBox.Open();
+            await _tcs.Task;
+        }
+
+        public static void DisplayMessage(State state)
+        {
+            Console.WriteLine(state == State.Ok ? "Операция была подтверждена" : "Операция была отклонена");
+            _tcs.SetResult();
         }
     }
 }
